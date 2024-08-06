66699cbf34718cb96795af5d57ff29d001b8fbef
Bhargav Gembali <113098503+BhargavGembali@users.noreply.github.com>
2024-08-06 12:08:01 +0530

    ui changes 2

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -20,7 +20,7 @@ const SearchPage = () => {
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for books..."
+          placeholder="Search by book titles..."
         />
         <button onClick={handleSearch}>Search</button>
       </div>
