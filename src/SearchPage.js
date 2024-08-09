0c4e425e9b163e3f1949e7521e858618ed1aa565
Bhargav Gembali <113098503+BhargavGembali@users.noreply.github.com>
2024-08-09 22:27:45 +0530

    commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,33 +5,53 @@ import './styles.css';
 const SearchPage = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [error, setError] = useState('');
 
-  const handleSearch = async () => {
+  // handleSearch function
+  const handleSearch = async (query) => {
     if (query.trim() !== '') {
-      const response = await axios.get(`https://book-search-backend-production-de33.up.railway.app/search`, { params: { query } });
-      setResults(response.data);
+      try {
+        const response = await axios.get('https://book-search-backend-production-de33.up.railway.app/search', { params: { query } });
+        setResults(response.data);
+        setError('');
+      } catch (error) {
+        console.error('Error fetching book links:', error.message);
+        setError('Failed to fetch book links. Please try again.');
+      }
+    } else {
+      setResults([]);
+      setError('');
     }
+  };
+
+  // Update the query state and trigger search on change
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+    handleSearch(value); // Trigger search immediately on each change
   };
 
   return (
     <div className="container">
       <div className="search-bar">
         <input
+          id="input"
           type="text"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={handleChange}
           placeholder="Search by book titles..."
         />
-        <button onClick={handleSearch}>Search</button>
+        <button onClick={() => handleSearch(query)}>Search</button>
       </div>
+      {error && <p className="error-message">{error}</p>}
       <div className="results-container">
         <ul className="results">
           {results.map((book, index) => (
             <li key={index} className="result-item">
               <h3>{book.title}</h3>
               <p>{book.author}</p>
-              <a href={book.link} target="_blank" rel="noopener noreferrer" className='View'>
-                View 
+              <a href={book.link} target="_blank" rel="noopener noreferrer" className="View">
+                View
               </a>
             </li>
           ))}
